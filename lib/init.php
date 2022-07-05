cba6fcf7bb9cd4bba522060beb8e6be06115a340
isaacbarnard <71669926+isaacbarnard@users.noreply.github.com>
2022-07-05 09:26:50 -0400

    Update init.php

--- lib/init.php
+++ lib/init.php
@@ -5,10 +5,10 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 define('__NAMESPACEROOT__', 	'resume');
-define('__HOMEPAGE__', 			'Home');
-define('__DIRROOT__', 			'/var/www/resume.wwdev.ca/');
-define('__IMAGEDIR__', 			__DIRROOT__ . 'public/assets/images/');
-define('__VIEWDIR__', 			__DIRROOT__ . 'view');
+define('__HOMEPAGE__', 		'Home');
+define('__DIRROOT__', 		'/var/www/resume.wwdev.ca/');
+define('__IMAGEDIR__', 		__DIRROOT__ . 'public/assets/images/');
+define('__VIEWDIR__', 		__DIRROOT__ . 'view');
 define('__CONTROLLERPATH__', 	__NAMESPACEROOT__ . '\\controllers\\');
 
 require_once(__DIR__ . '/helper.php');
